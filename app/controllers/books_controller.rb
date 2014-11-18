21e7e4418dd67760e384e06f1d5b05e93dd8d732
Philip Nguyen <supertaru@gmail.com>
2014-11-17 20:36:12 -0800

    Add create book API

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,4 +3,20 @@ class BooksController < ApplicationController
     books = Book.all
     render json: books, status: 200
   end
+
+  def create
+    book = Book.new(book_params)
+
+    if book.save
+      render json: book, status: 201, location: book_path(book)
+    else
+      render json: book.errors, status: 422
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :summary)
+  end
 end
--- test/features/book/book_test.rb
+++ test/features/book/book_test.rb
@@ -1,8 +1,8 @@
 require 'test_helper'
 
 class BookAPI < ActionDispatch::IntegrationTest
-  describe 'As an API consumer, I want to create a book' do
-    it 'Should create a new book' do
+  describe 'As an API consumer, I want to retrieve all books' do
+    it 'Should retrieve an index of all books' do
       get '/books'
 
       answer = JSON.parse(response.body, symbolize_names: true)[:books]
@@ -16,6 +16,44 @@ class BookAPI < ActionDispatch::IntegrationTest
       answer[1][:title].must_equal 'Wombat Returns'
       answer[1][:author].must_equal 'Joe Evermore'
       answer[1][:summary].must_equal 'Wombat returns to Goatham City'
+    end
+  end
+
+  describe 'As an API consumer, I want to create a book' do
+    it 'Should create a new book' do
+      post '/books', {
+        title: 'Superboy',
+        author: 'Rick Johnson',
+        summary: 'Klark Kentucky and Luisa Lane falls in love on a school trip'
+      }.to_json,
+      {
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+      }
+
+      answer = JSON.parse(response.body, symbolize_names: true)[:book]
+
+      response.status.must_equal 201
+      response.content_type.must_equal Mime::JSON
+      response.location.must_equal book_path Book.last
+      answer[:title].must_equal 'Superboy'
+      answer[:author].must_equal 'Rick Johnson'
+      answer[:summary].must_equal 'Klark Kentucky and Luisa Lane falls in love on a school trip'
+    end
+  end
+
+  describe 'As an API consumer, I want an error if creating an invalid book' do
+    it 'should response with a 422 status' do
+      post '/books', {
+        title: 'Badboy Badboy',
+        summary: 'Watcha gonna do? Watcha gonna do when we come for you?'
+      }.to_json,
+      {
+        'Accept' => 'application/json',
+        'Content-Type' => 'application/json'
+      }
+
+      response.status.must_equal 422
     end
   end
 end
