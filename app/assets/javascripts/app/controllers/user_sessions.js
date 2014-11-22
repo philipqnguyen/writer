ac10def9c21764029544f15f4a9f49f8c5bd0d20
Philip Nguyen <supertaru@gmail.com>
2014-11-22 13:33:22 -0800

    redirects after login and sign ups

--- app/assets/javascripts/app/controllers/user_sessions.js
+++ app/assets/javascripts/app/controllers/user_sessions.js
@@ -1,7 +1,17 @@
 (function () {
   var app = angular.module('Writer');
 
-  app.controller('UserSessionsCtrl', [function () {
+  app.controller('UserSessionsCtrl', ['$auth', '$location', function ($auth, $location) {
+    var self = this;
 
+    self.login = function () {
+      $auth.submitLogin(self.loginForm)
+        .then(function (resp) {
+          $location.path('/');
+        })
+        .catch(function (resp) {
+          console.log(resp);
+        });
+    };
   }]);
 }());
