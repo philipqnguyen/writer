8e27272958a9fc630471aa3b710ad6d1bad6fbd6
Philip Nguyen <supertaru@gmail.com>
2015-01-17 16:33:55 -0800

    Add styles

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -5,7 +5,7 @@
   var app = angular.module('Writer', ['ngRoute', 'ng-token-auth']);
 
   app.config(['$httpProvider', function ($httpProvider) {
-    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token').attr('content');
+    $httpProvider.defaults.headers.common['X-CSRF-Token'] = $('meta[name=csrf-token]').attr('content');
     $httpProvider.defaults.headers.common.Accept = 'application/json';
   }]);
 
